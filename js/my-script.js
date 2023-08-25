5837eeb0b9ad170e852d2ab4fdd0964a82d0671f
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-08-25 16:25:28 +0200

    EDIT bonus completato

--- js/my-script.js
+++ js/my-script.js
@@ -5,10 +5,28 @@ createApp({
         return{
 
             database: '' ,
+            showModalFlag: false,
+            modalObj:'',
 
         }
     },
     methods:{
+
+        showModal(index){
+
+            this.showModalFlag=true
+
+            axios.get("http://localhost/php-dischi-json/api.php")
+            .then((res)=>{
+    
+                console.log(res.data)
+                
+                this.modalObj = res.data.discs[index]
+                
+            })
+
+        }
+
         
     },
     created(){
